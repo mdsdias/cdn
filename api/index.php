6f36c94d074d9f702b417f2be5e030526539ee4a
Isqne <isqne@pm.me>
2021-09-18 13:25:01 -0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,5 @@
+<form method="post" enctype="multipart/form-data" action="upload.php">
+   Selecione uma imagem: <input name="arquivo" type="file" />
+   <br />
+   <input type="submit" value="Salvar" />
+</form>
