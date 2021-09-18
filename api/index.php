45656a80a0a72c192c39317c68f5f8dddfd19e1d
Isqne <isqne@pm.me>
2021-09-18 13:27:44 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
-<form method="post" enctype="multipart/form-data" action="upload.php">
+<form method="post" enctype="multipart/form-data" action="./upload.php">
    Selecione uma imagem: <input name="arquivo" type="file" />
    <br />
    <input type="submit" value="Salvar" />
