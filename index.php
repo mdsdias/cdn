e13729eff5e9d46871b3dc3883fbf9c107863790
Isqne <isqne@pm.me>
2021-09-18 13:25:20 -0300

    Be like :D

--- index.php
+++
@@ -1,5 +0,0 @@
-<form method="post" enctype="multipart/form-data" action="upload.php">
-   Selecione uma imagem: <input name="arquivo" type="file" />
-   <br />
-   <input type="submit" value="Salvar" />
-</form>
